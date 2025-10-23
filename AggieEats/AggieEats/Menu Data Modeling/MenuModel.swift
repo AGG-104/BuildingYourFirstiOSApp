7cc2e97025ab9cab2c29597ef976fcae0fc83c30
AGG-104 <gamber104@gmail.com>
2025-10-23 16:56:06 -0700

    stage changes

---
+++ AggieEats/AggieEats/Menu Data Modeling/MenuModel.swift
@@ -0,0 +1,12 @@
+//
+//  MenuModel.swift
+//  AggieEats
+//
+//  Created by Amber Gonzalez on 10/23/25.
+//
+
+import Foundation
+
+struct Menu: Codable, Hashable {
+    //TODO: Model JSON Data in Swift.
+}
